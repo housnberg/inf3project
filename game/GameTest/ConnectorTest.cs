f172805514e24a77dbbfac64bbe17a35ecbb7887
Eugen L. <eugen.lj93@gmail.com>
2014-10-18 18:30:06 +0200

    added new solution for unit-testing

---
+++ game/GameTest/ConnectorTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using game.client;
+
+namespace GameTest
+{
+    [TestClass]
+    public class ConnectorTest
+    {
+        [TestMethod]
+        public void connectTest()
+        {
+            Boolean expected = true;
+            Connector connection = new Connector("127.0.0.1", 1024);
+            Assert.AreEqual(expected, connection.getTcpClient().Connected);
+        }
+
+        [TestMethod]
+        public void disconnectTest()
+        {
+            Boolean expected = false;
+            Connector connection = new Connector("127.0.0.1", 1024);
+            connection.disconnect();
+            Assert.AreEqual(expected, connection.getTcpClient().Connected);
+        }
+
+        [TestMethod]
+        public void sendServerMessageTest()
+        {
+            Connector connection = new Connector("127.0.0.1", 1024);
+            connection.sendServerMessage("HalloTest");
+        }
+
+    }
+}
