70b0234ee4be4f4fc73beea5ebfa761af452f4f1
Eugen L. <eugen.lj93@gmail.com>
2014-10-18 17:28:50 +0200

    updated main-method for local usage of the project

---
+++ game/game/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using game.client;
+
+namespace game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ip for localhost, port 1024 for local server via eclips
+            Connector client = new Connector("127.0.0.1", 1024);
+            Console.WriteLine("success!");
+            Console.ReadKey();
+        }
+    }
+}
