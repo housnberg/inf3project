f0872bd936bf8e13b0d54a68f37e540ab0fe2e42
Kosta A. <kosta.anagnos@gmail.com>
2014-10-19 18:54:48 +0200

    GameManager-Implementation: sendCommand()-method

--- game/game/GameManager.cs
+++ game/game/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using game.client;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,34 @@ namespace game
 {
     class GameManager
     {
-     private Map map;
-        
+        private Map map;
+        private ArrayList players;
+        private ArrayList dragons;
+        private int numberOfPlayer;
+
+        public void startGame()
+        {
+
+        }
+
+         public int getNumberOfPlayers()
+         {
+             return numberOfPlayer;
+         }
+
+         public void setNumberOfPlayers()
+         {
+
+         }
+
+         /// <summary>
+         /// Sends a message to the Connector to perform a certain action
+         /// </summary>
+         /// <param name="connector">The actual game connector</param>
+         /// <param name="message">The message being sent to the server via the connector</param>
+        public void sendCommand(Connector connector,String message){
+            connector.sendServerMessage(message);
+        }
         
     }
 }
