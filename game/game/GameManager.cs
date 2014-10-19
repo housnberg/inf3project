e05ef34b9e0aa2d80204392627d7eb350f5ed64e
Kosta A. <kosta.anagnos@gmail.com>
2014-10-19 18:40:19 +0200

    Map Class implemented

--- game/game/GameManager.cs
+++ game/game/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace game
 {
     class GameManager
     {
+     private Map map;
+        
+        
     }
 }
--- game/game/Map.cs
+++ game/game/Map.cs
@@ -8,5 +8,59 @@ namespace game
 {
     class Map
     {
+        private int width;
+        private int height;
+        private Field[,] fields; //2Dimensional Array
+
+        public Map(int width, int height)
+        {
+            this.setWidth(width);
+            this.setHeight(height);
+            fields = new Field[height,width]; // [x,y] --> NOT [x][y]
+        }
+
+        /// <summary>
+        /// Checks wether the width is allowed or not
+        /// </summary>
+        /// <param name="width">Width of the map</param>
+        private void setWidth(int width)
+        {
+            if (width < 5 || width > 30)
+            {
+                throw new Exception("Breite kann nicht kleiner als 5 oder größer als 30 sein!");
+            }
+            this.width = width;
+        }
+
+        /// <summary>
+        /// Checks wether the height is allowed or not
+        /// </summary>
+        /// <param name="height">Height of the map</param>
+        private void setHeight(int height)
+        {
+            if (height < 5 || height > 30)
+            {
+                throw new Exception("Höhe kann nicht kleiner als 5 oder größer als 30 sein!");
+            }
+            this.height = height;
+        }
+
+        /// <summary>
+        /// Return the height of the Map as an int Value
+        /// </summary>
+        /// <returns>Heigth of the Map </returns>
+        public int getHeight()
+        {
+            return this.height;
+        }
+
+        /// <summary>
+        /// Return the width of the Map as an int Value
+        /// </summary>
+        /// <returns>Width of the Map </returns>
+        public int getWidth()
+        {
+            return this.width;
+        }
     }
 }
