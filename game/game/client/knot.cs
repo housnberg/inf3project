160dc1f61fc156dbd08a5c488f41a1a7de0bf059
schneider1991 <david.schneider91@gmx.de>
2014-10-21 22:36:37 +0200

    new Buffer

---
+++ game/game/client/knot.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game.client
+{
+    class Knot
+    {
+        private String s;
+        private Knot next;
+
+
+        public Knot(String s, Knot next)
+        {
+            this.setData(s);
+            this.setNext(next);
+        }
+
+        public String getData()
+        {
+            return s;
+        }
+
+        public void setData(String s)
+        {
+            this.s = s;
+        }
+
+        public Knot getNext()
+        {
+            return next;
+        }
+
+        public void setNext(Knot next)
+        {
+            this.next = next;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
