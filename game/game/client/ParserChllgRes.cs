6b9cabbff1eeea718d54bb67c3baa09e6fdab202
reptilehead (Johann A.) <johgimaster@web.de>
2014-10-22 16:50:54 +0200

    Added ParserChllgRes class. It will parse challenge and result messages. Basic methods included.

---
+++ game/game/client/ParserChllgRes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace game.client
+{
+    class ParserChllgRes
+    {
+        ParserGate pg;
+        String msg;
+        bool msg_is_vld;
+
+        /// <summary>
+        /// Parses the message applying the "CHALLENGE" rule. Will eventually call the ParserToken or ParserMap class.
+        /// </summary>
+        /// <param name="msg">Part of original message, is expected to fit the "CHALLANGE" rule</param>
+        void parseChllg(String msg)
+        {
+            Contract.Requires(msg != null && msg_is_vld);
+            Contract.Ensures(msg_is_vld);
+        }
+
+        /// <summary>
+        /// Parses the message applying the "RESULT" rule. Will eventually call the ParserToken or ParserMap class.
+        /// </summary>
+        /// <param name="msg">Part of original message, is expected to fit the "RESULT" rule</param>
+        void parseRes(String msg)
+        {
+            Contract.Requires(msg != null && msg_is_vld);
+            Contract.Ensures(msg_is_vld);
+        }
+
+        /// <summary>
+        /// Parses the message applying the "OPPONENT" rule. Will eventually call the ParserToken or ParserMap class.
+        /// </summary>
+        /// <param name="msg">Part of original message, is expected to fit the "OPPONENT" rule</param>
+        void parseOpp(String msg)
+        {
+            Contract.Requires(msg != null && msg_is_vld);
+            Contract.Ensures(msg_is_vld);
+        }
+    }
+}
