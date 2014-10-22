acdb1bcba129c84e3805b8137a932d967d82386f
reptilehead (Johann A.) <johgimaster@web.de>
2014-10-22 16:44:01 +0200

    Created new Class ParserUpdDel. As the name implies, it parses update and delete messages.

---
+++ game/game/client/ParserUpdDel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace game.client
+{
+    class ParserUpdDel
+    {
+
+        ParserGate pg;
+        String msg;
+        bool msg_is_vld;
+
+        /// <summary>
+        /// Parses the message applying the "UPDATE" rule. Will eventually call the ParserToken or ParserMap class.
+        /// </summary>
+        /// <param name="msg">Part of original message, is expected to fit the "UPDATE" rule</param>
+        void parseUpd(String msg)
+        {
+            Contract.Requires(msg != null && msg_is_vld);
+            Contract.Ensures(msg_is_vld);
+        }
+
+        /// <summary>
+        /// Parses the message applying the "DELETE" rule. Will eventually call the ParserToken class.
+        /// </summary>
+        /// <param name="msg">Part of original message, is expected to fit the "DELETE" rule</param>
+        void parseDel(String msg)
+        {
+            Contract.Requires(msg != null && msg_is_vld);
+            Contract.Ensures(msg_is_vld);
+        }
+    }
+}
