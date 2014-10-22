938ea820ba6a58d1ea56857ba643b8ad805ce303
reptilehead (Johann A.) <johgimaster@web.de>
2014-10-22 17:00:41 +0200

    Added new ParserToken class. It will parse player and dragon messages. Basic methods included.
    Also small doc corrections.

--- game/game/client/ParserChllgRes.cs
+++ game/game/client/ParserChllgRes.cs
@@ -16,7 +16,7 @@ namespace game.client
         /// <summary>
         /// Parses the message applying the "CHALLENGE" rule.
         /// </summary>
-        /// <param name="msg">Part of original message, is expected to fit the "CHALLANGE" rule</param>
+        /// <param name="msg">Part of original message, is expected to fit the "CHALLANGE" rule.</param>
         void parseChllg(String msg)
         {
             Contract.Requires(msg != null && msg_is_vld);
@@ -26,7 +26,7 @@ namespace game.client
         /// <summary>
         /// Parses the message applying the "RESULT" rule. Will eventually call the parseOpp(String msg) method.
         /// </summary>
-        /// <param name="msg">Part of original message, is expected to fit the "RESULT" rule</param>
+        /// <param name="msg">Part of original message, is expected to fit the "RESULT" rule.</param>
         void parseRes(String msg)
         {
             Contract.Requires(msg != null && msg_is_vld);
@@ -36,7 +36,7 @@ namespace game.client
         /// <summary>
         /// Parses the message applying the "OPPONENT" rule. Will eventually call the ParserToken or ParserMap class.
         /// </summary>
-        /// <param name="msg">Part of original message, is expected to fit the "OPPONENT" rule</param>
+        /// <param name="msg">Part of original message, is expected to fit the "OPPONENT" rule.</param>
         void parseOpp(String msg)
         {
             Contract.Requires(msg != null && msg_is_vld);
---
+++ game/game/client/ParserToken.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace game.client
+{
+    class ParserToken
+    {
+
+        ParserGate pg;
+        String msg;
+        bool msg_is_vld;
+
+        /// <summary>
+        /// Parses the message applying the "PLAYER" rule.
+        /// </summary>
+        /// <param name="p_msg">Part of original message, is expected to fit the "PLAYER" rule:</param>
+        void parsePlayer(String p_msg)
+        {
+            Contract.Requires(p_msg != null && msg_is_vld);
+            Contract.Ensures(msg_is_vld);
+        }
+
+        /// <summary>
+        /// Parses the message applying the "DRAGON" rule.
+        /// </summary>
+        /// <param name="p_msg">Part of original message, is expected to fit the "DRAGON" rule:</param>
+        void parseDrgn(String p_msg)
+        {
+            Contract.Requires(p_msg != null && msg_is_vld);
+            Contract.Ensures(msg_is_vld);
+        }
+    }
+}
