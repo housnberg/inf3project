77cc5ecfdb35bea5ad022b5cffa8ed0a73252691
Kosta A. <kosta.anagnos@gmail.com>
2014-10-23 14:43:43 +0200

    Added UnitTests to GameManagerClass

---
+++ game/game/GUI/Gui.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game.GUI
+{
+    class Gui
+    {
+        public Gui()
+        {
+
+        }
+
+        /// <summary>
+        /// Forces GUI to reload
+        /// </summary>
+        /// <param name="map">Map of the game</param>
+        public void reloadGui(Map map)
+        {
+            Contract.Requires(map != null);
+        }
+    }
+}
