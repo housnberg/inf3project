2f86a9498a29ad4ba69be2cc353dbc9f1072cbd4
reptilehead (Johann A.) <johgimaster@web.de>
2014-10-29 12:41:14 +0100

    Created get-/set-methods for the ParserGate.

--- game/game/Parser/ParserGate.cs
+++ game/game/Parser/ParserGate.cs
@@ -13,26 +13,81 @@ namespace game.Parser
         private ClientBuffer buffer;
         private String message;
         private bool messageIsValid;
-        private bool messageIsAvailable;
 
         /// <summary>
         /// Receives incoming messages from the buffer.
         /// Ensures that incoming messages from the server contain valid information.
         /// The information is then extracted and passed down to the GameManager
         /// </summary>
-        public ParserGate()
+        public ParserGate(ClientBuffer buffer)
         {
 
         }
 
         /// <summary>
+        /// Getter for the buffer variable.
+        /// </summary>
+        /// <returns>Returns the value of the buffer variable.</returns>
+        ClientBuffer getBuffer()
+        {
+            return this.buffer;
+        }
+
+        /// <summary>
+        /// Getter for the messageIsValid variable.
+        /// </summary>
+        /// <returns>Returns the value of the messageIsValid variable.</returns>
+        bool getMessageIsValid()
+        {
+            return this.messageIsValid;
+        }
+
+        /// <summary>
+        /// Getter for the message variable.
+        /// </summary>
+        /// <returns>Returns the value of the message variable.</returns>
+        String getMessage()
+        {
+            return this.message;
+        }
+
+        /// <summary>
+        /// Setter for the buffer variable. The new value is only set if buffer != null.
+        /// </summary>
+        /// <param name="buffer">The ClientBuffer Object to which the parser will listen to.</param>
+        private void setBuffer(ClientBuffer buffer)
+        {
+            if (buffer != null)
+                this.buffer = buffer;
+        }
+
+        /// <summary>
+        /// Setter for the messageIsValid variable. The new value is only set if validity != null.
+        /// </summary>
+        /// <param name="validity">Bool variable expressing the current state (true = valid, false = invalid) of the message is the parsing process.</param>
+        private void setMessageIsValid(bool validity)
+        {
+            if (validity != null)
+                this.messageIsValid = validity;
+        }
+
+        /// <summary>
         /// Gets one full message from the buffer, if one is available, and stores it in the message variable.
         /// </summary>
-        public void getMessage()
+        private void extractMessage()
         {
-            Contract.Requires(messageIsAvailable);
+            // Contract.Requires(messageIsAvailable);
             Contract.Ensures(message != null);
         }
+
+        /// <summary>
+        /// This method will be constantly asking the buffer is a message is available. If there is one, it will call extractMessage().
+        /// </summary>
+        private void listenToBuffer()
+        {
+
+        }
+
 
         /// <summary>
         /// Parses the message and extracts information from it. Will call other methods, choosing based on message content.
