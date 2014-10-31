4a1e38021449b1f5bdacf8add3f6aa1c28e63cb0
Eugen L. <eugen.lj93@gmail.com>
2014-10-31 20:24:32 +0100

    added new classes Receiver and Sender

---
+++ game/game/client/Receiver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace game.client
+{
+    public class Receiver
+    {
+        private TcpClient client;
+
+        public Receiver(TcpClient client)
+        {
+            setTcpClient(client);
+        }
+
+        private void setTcpClient(TcpClient client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException("the parameter cannot be null");
+            }
+            this.client = client;
+        }
+
+        /// <summary>
+        /// receives messages from the server permanently
+        /// </summary>
+        public String receive()
+        {
+            return "";
+        }
+    }
+}
---
+++ game/game/client/Sender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace game.client
+{
+    public class Sender
+    {
+        private TcpClient client;
+
+        public Sender(TcpClient client)
+        {
+            setTcpClient(client);
+        }
+
+        private void setTcpClient(TcpClient client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException("the parameter cannot be null");
+            }
+            this.client = client;
+        }
+
+        /// <summary>
+        /// sends messages to the server
+        /// </summary>
+        /// <param name="message">the message to be send</param>
+        public void send(String message)
+        {
+
+        }
+    }
+}
