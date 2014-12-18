76227b63e084e010bb681e713ce84da7a7cdc127
reptilehead (Johann A.) <johgimaster@web.de>
2014-12-18 14:23:53 +0100

    Created Observer & Observable interfaces.

---
+++ game/game/backend/Observable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game.backend
+{
+    interface Observable
+    {
+        List<Observer> observerList;
+
+        public void addObserver();
+        public void deleteObserver();
+        public void upDate();
+    }
+}
---
+++ game/game/backend/Observer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game.backend
+{
+    interface Observer
+    {
+        Player observedPlayer;
+        public void getUpdate();
+    }
+}
