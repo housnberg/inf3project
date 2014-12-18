d5d65d2610b4c89273c1312f3e3bc2de0648f7f2
reptilehead (Johann A.) <johgimaster@web.de>
2014-12-18 23:58:10 +0100

    created documentation

--- game/game/backend/Player.cs
+++ game/game/backend/Player.cs
@@ -22,43 +22,70 @@ namespace game
 
 
 
-
-        public new void setID(int id)
+        /// <summary>
+        /// New setter, calls the IdChange method.
+        /// </summary>
+        /// <param name="id">the new id</param>
+        public void setID(int id)
         {
             base.setID(id);
             this.IdChange();
         }
 
+        /// <summary>
+        /// New setter, calls the IsBusyChange method.
+        /// </summary>
+        /// <param name="busy">the new busy status</param>
         public void setBusy(bool busy)
         {
             base.setBusy(busy);
             this.IsBusyChange();
         }
 
+        /// <summary>
+        /// New setter, calls the DescChange() method at the end.
+        /// </summary>
+        /// <param name="desc">the new description</param>
         public void setDesc(String desc)
         {
             base.setDesc(desc);
             this.DescChange();
         }
 
+        /// <summary>
+        /// New setter, calls the PosChange() method at the end.
+        /// </summary>
+        /// <param name="xPos">new x-Position</param>
         public void setXPos(int xPos)
         {
             base.setXPos(xPos);
             this.PosChange();
         }
 
+        /// <summary>
+        /// New setter, calls the PosChange() method at the end.
+        /// </summary>
+        /// <param name="yPos">the new y-Position</param>
         public void setYPos(int yPos)
         {
             base.setYPos(yPos);
             this.PosChange();
         }
 
+        /// <summary>
+        /// New setter, calls the PointChange() method at the end.
+        /// </summary>
+        /// <param name="points">the new points</param>
         public void setPoints(int points)
         {
             base.setPoints(points);
             this.PointChange();
         }
 
+        /// <summary>
+        /// Registers a PlayerObserver object.
+        /// </summary>
+        /// <param name="observer">the PlayerObserver object.</param>
         public void addObserver(PlayerObserver observer)
         {
             if (observer != null)
@@ -68,6 +95,10 @@ namespace game
 
         }
 
+        /// <summary>
+        /// Delets a single PlayerObserver object.
+        /// </summary>
+        /// <param name="observer">The PlayerObserver object to be deleted.</param>
         public void deleteObserver(PlayerObserver observer)
         {
             if (observer != null)
@@ -79,6 +110,9 @@ namespace game
             }
         }
 
+        /// <summary>
+        /// Deletes all PlayerObserver objects.
+        /// </summary>
         public void deleteAllObservers()
         {
             if (this.observerList != null)
@@ -90,6 +124,9 @@ namespace game
             }
         }
 
+        /// <summary>
+        /// Notifies the observer about a new Player id.
+        /// </summary>
         public void IdChange()
         {
             if (this.observerList != null)
@@ -104,6 +141,9 @@ namespace game
             }
         }
 
+        /// <summary>
+        /// Notifies the observer about a new busy status.
+        /// </summary>
         public void IsBusyChange()
         {
             if (this.observerList != null)
@@ -118,6 +158,9 @@ namespace game
             }
         }
 
+        /// <summary>
+        /// Notifies the observer about a new player description.
+        /// </summary>
         public void DescChange()
         {
             if (this.observerList != null)
@@ -132,6 +175,9 @@ namespace game
             }
         }
 
+        /// <summary>
+        /// Notifies the Observer about a change in player position.
+        /// </summary>
         public void PosChange()
         {
             if (this.observerList != null)
@@ -146,6 +192,9 @@ namespace game
             }
         }
 
+        /// <summary>
+        /// Notifies the observer about a change in points.
+        /// </summary>
         public void PointChange()
         {
             if (this.observerList != null)
--- game/game/backend/PlayerObserver.cs
+++ game/game/backend/PlayerObserver.cs
@@ -11,27 +11,42 @@ namespace game.backend
         PathWalker pwInstance = PathWalker.getPathWalkerInstance();
         GameManager gameManager = GameManager.getGameManagerInstance();
 
+        /// <summary>
+        /// Takes action according to the new change
+        /// </summary>
         public void onIdChange()
         {
             Console.WriteLine("Player ID has changed.");
         }
 
+        /// <summary>
+        /// Takes action according to the new change
+        /// </summary>
         public void onIsBusyChange()
         {
             Console.WriteLine("Player isBusy has changed.");
         }
 
+        /// <summary>
+        /// Takes action according to the new change
+        /// </summary>
         public void onDescChange()
         {
             Console.WriteLine("Player description has changed");
         }
 
+        /// <summary>
+        /// Takes action according to the new change
+        /// </summary>
         public void onPosChange()
         {
             Console.WriteLine("Player position has changed.");
             pwInstance.walk(gameManager.getThisPlayer().getYPos(), gameManager.getThisPlayer().getXPos());
         }
 
+        /// <summary>
+        /// Takes action according to the new change
+        /// </summary>
         public void onPointChange()
         {
             Console.WriteLine("Player points have changed.");
