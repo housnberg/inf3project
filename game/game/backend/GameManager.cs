3e892c8473ba1936f6921b09ad6208b48de0aea2
reptilehead (Johann A.) <johgimaster@web.de>
2014-12-21 11:07:50 +0100

    merge problems

--- game/game/backend/GameManager.cs
+++ game/game/backend/GameManager.cs
@@ -406,8 +406,14 @@ namespace game
                     }
                     else if (players.Count > 0 && players[count].getID() == player.getID())
                     {
-                        players[count].setXPos(player.getXPos());
-                        players[count].setYPos(player.getYPos());
+                        if (players[count].getYPos() == player.getYPos())
+                        {
+                            players[count].setXPos(player.getXPos());
+                        }
+                        else if (players[count].getXPos() == player.getYPos())
+                        {
+                            players[count].setYPos(player.getYPos());
+                        }                       
                         found = true;
                     }
                     else
