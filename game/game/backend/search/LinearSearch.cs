24021e4d81693243da4a09842ac0c967cbc5584c
Eugen L. <eugen.lj93@gmail.com>
2015-01-15 16:06:11 +0100

    included LinearSearchClass

---
+++ game/game/backend/search/LinearSearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAndSort
+{
+    class LinearSearch<C>
+    {
+        /// <summary>
+        /// Linear search through a list. Searches as long as the list is not exhausted and the searched element is not found yet.
+        /// </summary>
+        /// <param name="list">the list to search through</param>
+        /// <param name="criterion">the criterion from which we determine, whether we have found our element</param>
+        /// <returns>the element, if it is found, or the default value of type C, if not</returns>
+        public C find(List<C> list, Func<C, bool> criterion)
+        {
+            C result = default(C);
+            int i = 0;
+            while (i < list.Count)
+            {
+                if(criterion(list[i])) {
+                    result = list[i];
+                }
+                i++;
+            }
+            return result;
+        }
+    }
+}
